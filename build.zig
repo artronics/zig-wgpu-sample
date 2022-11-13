10a24a7e4e2f8c26bd5f57f75bb3c365ff72fb30
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-11-13 01:54:26 +0000

    add gamedev dependencies

--- build.zig
+++ build.zig
@@ -1,19 +1,30 @@
 const std = @import("std");
+const zgpu = @import ("./zig-gamedev/libs/zgpu/build.zig");
+const zmath = @import("./zig-gamedev/libs/zmath/build.zig");
+const zpool = @import("./zig-gamedev/libs/zpool/build.zig");
+const zglfw = @import("./zig-gamedev/libs/zglfw/build.zig");
+const zgui = @import ("./zig-gamedev/libs/zgui/build.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("wgpu-ui-hello", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+
+    const zgpu_options = zgpu.BuildOptionsStep.init(b, .{});
+    const zgpu_pkg = zgpu.getPkg(&.{ zgpu_options.getPkg(), zpool.pkg, zglfw.pkg });
+
+    exe.addPackage(zgpu_pkg);
+    exe.addPackage(zgui.pkg);
+    exe.addPackage(zmath.pkg);
+    exe.addPackage(zglfw.pkg);
+
+    zgpu.link(exe, zgpu_options);
+    zglfw.link(exe);
+    zgui.link(exe);
+
     exe.install();
 
     const run_cmd = exe.run();
@@ -31,4 +42,8 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
+}
+
+inline fn thisDir() []const u8 {
+    return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
